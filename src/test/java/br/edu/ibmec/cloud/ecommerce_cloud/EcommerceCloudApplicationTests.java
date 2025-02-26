624e071a101c3807bc04674ec7e133bd24eec933
Rafael Cruz <rafaelcruz.net81@gmail.com>
2025-02-26 20:51:57 -0300

    Criação do projeto

---
+++ src/test/java/br/edu/ibmec/cloud/ecommerce_cloud/EcommerceCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ibmec.cloud.ecommerce_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

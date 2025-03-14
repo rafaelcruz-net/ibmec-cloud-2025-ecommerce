0d640d57bf2bb497d746076e416c5150ba5196d5
Rafael Cruz <rafaelcruz.net81@gmail.com>
2025-03-14 10:28:14 -0300

    Criação da API para autorizar a compra

--- src/main/java/br/edu/ibmec/cloud/ecommerce_cloud/controller/CartaoController.java
+++ src/main/java/br/edu/ibmec/cloud/ecommerce_cloud/controller/CartaoController.java
@@ -46,4 +46,74 @@ public class CartaoController {
 
     }
 
+    @PostMapping("/authorize")
+    public ResponseEntity<TransacaoResponse> authorize(@PathVariable("id_user") int id_user,
+                                                       @RequestBody TransacaoRequest request) {
+        //Verificando se o usuario existe na base
+        Optional<Usuario> optionalUsuario = this.usuarioRepository.findById(id_user);
+
+        if (optionalUsuario.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        Usuario user = optionalUsuario.get();
+        Cartao cartaoTransacao = null;
+
+        for (Cartao cartao: user.getCartoes()) {
+            if (cartao.getNumero().equals(request.getNumero()) && cartao.getCvv().equals(request.getCvv())) {
+                cartaoTransacao = cartao;
+                break;
+            }
+        }
+
+        //Não achei o cartao associado para o usuario
+        if (cartaoTransacao == null) {
+            TransacaoResponse response = new TransacaoResponse();
+            response.setStatus("NOT_AUTHORIZED");
+            response.setDtTransacao(LocalDateTime.now());
+            response.setMessage("Cartão não encontrado para o usuario");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+
+        //Verifica se o cartao não está expirado
+        if (cartaoTransacao.getDtExpiracao().isBefore(LocalDateTime.now())) {
+            TransacaoResponse response = new TransacaoResponse();
+            response.setStatus("NOT_AUTHORIZED");
+            response.setDtTransacao(LocalDateTime.now());
+            response.setMessage("Cartão Expirado");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        //Verifica se tem dinheiro no cartao para realizr a compra
+        if (cartaoTransacao.getSaldo() < request.getValor()) {
+            TransacaoResponse response = new TransacaoResponse();
+            response.setStatus("NOT_AUTHORIZED");
+            response.setDtTransacao(LocalDateTime.now());
+            response.setMessage("Sem saldo para realizar a compra");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        //Pega o saldo do cartão
+        Double saldo = cartaoTransacao.getSaldo();
+
+        //Substrai o saldo com o valor da compra
+        saldo = saldo - request.getValor();
+
+        //Atualiza o saldo do cartao
+        cartaoTransacao.setSaldo(saldo);
+
+        //Grava o novo saldo na base de dados
+        this.cartaoRepository.save(cartaoTransacao);
+
+        //Compra Autorizada
+        TransacaoResponse response = new TransacaoResponse();
+        response.setStatus("AUTHORIZED");
+        response.setDtTransacao(LocalDateTime.now());
+        response.setMessage("Compra autorizada");
+        response.setCodigoAutorizacao(UUID.randomUUID());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
+    }
+
+
 }
